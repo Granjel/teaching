ef6a4e112139ac30e520b7a04cd3197be81f6168
Rodrigo Granjel <granjel@gmail.com>
2025-02-27 12:34:40 +0100

    final version 2025-fourth-corner

---
+++ fourth-corner/load_new_data.R
@@ -0,0 +1,7 @@
+# load packages
+library(tidyverse)
+library(readxl)
+
+# load the data and saving it as the different matrices
+data <- read.csv("fourth-corner/data/Abundance_data_trait_info_2024_25(Abundance).csv", header = TRUE, sep = ";")
+
